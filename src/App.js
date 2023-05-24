5d66af18ed39fcbacd92aa2881aa2100370da452
Harshit <harshitgupta4426@gmail.com>
2023-05-24 15:16:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-// import About from './Components/About';
+import About from './Components/About';
+import Alert from './Components/Alert';
 import TextForm from './Components/TexForm'; 
+import React,{ useState} from 'react'
+import { BrowserRouter,Routes,Route, } from 'react-router-dom';
 
 function App() {
+const [mode,  setMode] = useState('light');
+const [alert, setalert] = useState(null);
+
+const showAlert = (message,type)=>{
+  setalert({
+      msg : message,
+      type: type  
+  })
+  setTimeout(() => {
+    setalert(null)
+  }, 1500);
+}
+  const togglemode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.background = '#042743'
+      showAlert("Dark mode has been enabled","success")
+    }
+    else{
+      setMode('light');
+      document.body.style.background = 'white'
+      showAlert("light mode has been enabled","success")
+    }
+  }
   return ( 
     <>
-<Navbar text="TextUtils"/>
+
+
+<Alert alert={alert} />
 <div className="container my-3">
-<TextForm  heading="Enter the text to analyz"/>
+<BrowserRouter>
+<Navbar text="TextUtils" mode={mode} togglemode={togglemode} aboutText="About"/>
+  <Routes>
+    <Route path = "/" element={<TextForm showAlert={showAlert} heading= "Enter the text to analyz below " mode={mode}/>} />
+    <Route path="about" element={<About/>}/>
+  </Routes>
+  
+</BrowserRouter>
 
-</div>
-{/* <About /> */}
-
+</div> 
 </>
-  );
+);
 }
 
 export default App;
--- src/Components/TexForm.js
+++ src/Components/TexForm.js
@@ -6,14 +6,17 @@ export default function TexForm(props) {
    const handelUpClick=() =>{
      let newText = text.toUpperCase();
      setText(newText)
+     props.showAlert("Converted to uppercase","success")
     }
     const handelLoClick=() =>{
       let newText = text.toLowerCase();
       setText(newText)
+      props.showAlert("Converted to lowercase","success")
      }
      const handelClearClick=() =>{
       let newText = '';
       setText(newText)
+      props.showAlert("Text cleared ","success")
      }
 
     const handelOnChange=(event) =>{ 
@@ -22,7 +25,13 @@ export default function TexForm(props) {
        const handelCopy= () =>{
          var text = document.getElementById("myBox");
          text.select();
-        navigator.clipboard.writeText(text.value);  
+        navigator.clipboard.writeText(text.value); 
+        props.showAlert("copy to clipboard","success") 
+        }
+        const handelExtraSpaces = () => {
+          let newText = text.split(/[ ]+/);
+          setText(newText.join(" "))
+          props.showAlert("Extra space is removed","success") 
         }
     
     const [text ,setText]= useState('')
@@ -37,24 +46,26 @@ export default function TexForm(props) {
   }
   return (
     <>
-    <div className="container">
+    <div className="container" style ={{color:props.mode=== 'dark'?'white':'#042743'}}>
     <h1>{props.heading}</h1>
       <div className="mb-3">
-      <textarea className="form-control" id="myBox" value={text} onChange={handelOnChange}  rows="8"></textarea>
+      <textarea className="form-control" id="myBox" value={text} onChange={handelOnChange} style ={{background:props.mode=== 'dark'?'grey':'white',color:props.mode=== 'dark'?'white':'#042743'}}  rows="8"></textarea>
       </div>
       <button className="btn btn-primary mx-2" onClick={handelUpClick}>Convert to Uppercase </button>
       <button className="btn btn-primary mx-2" onClick={handelLoClick}>Convert to Lowercase </button>
       <button className="btn btn-primary mx-2" onClick={handelClearClick}>Clear</button>
       <button className="btn btn-primary mx-2" onClick={handelCopy}>Copy text</button>
+      <button className="btn btn-primary mx-2" onClick={handelExtraSpaces}>Remove  Extra Spaces</button>
+
 
 
     </div>
-    <div className="container my-2">
+    <div className="container my-2" style ={{color:props.mode=== 'dark'?'white':'#042743'}}>
         <h2>Your text summary</h2>
         <p>{countWords(text)} Words {countCharacters(text)} Characters</p>
         <p>{0.008*countWords(text)} Minutes to read</p>
         <h2>preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter something in the textbox above to preview it"}</p>
     </div>
     
     </>
